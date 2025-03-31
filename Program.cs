e28abe6750f916c6d61c76568d261f4f1c948e82
nlutala <nathanlutala@outlook.com>
2025-03-31 11:54:08 +0100

    Using the Trace Class

--- Program.cs
+++ Program.cs
@@ -8,39 +8,19 @@ class Program
         int iUnitQty = 100;
         double dUnitCost = 1.03;
 
-        Debug.WriteLine("Debug Information-Product Starting ");
-        Debug.Indent();
+        Trace.WriteLine("Trace Information-Product Starting ");
+        Trace.Indent();
 
-        /* Debug.WriteLine("The product name is " + sProdName);
-        Debug.WriteLine("The available units on hand are " + iUnitQty.ToString());
-        Debug.WriteLine("The per unit cost is " + dUnitCost.ToString()); */
+        Trace.WriteLine("The product name is " + sProdName);
+        Trace.WriteLine("The product name is" + sProdName, "Field");
+        Trace.WriteLineIf(iUnitQty > 50, "This message WILL appear");
+        Trace.Assert(dUnitCost > 1, "Message will NOT appear");
 
-        Debug.WriteLine("The product name is " + sProdName, "Field");
-        Debug.WriteLine("The units on hand are " + iUnitQty, "Field");
-        Debug.WriteLine("The per unit cost is " + dUnitCost.ToString(), "Field");
-        Debug.WriteLine("Total Cost is " + (iUnitQty * dUnitCost), "Calc");
+        Trace.Unindent();
+        Trace.WriteLine("Trace Information-Product Ending");
 
-        Debug.WriteLineIf(iUnitQty > 50, "This message WILL appear");
-        Debug.WriteLineIf(iUnitQty < 50, "This message will NOT appear");
+        Trace.Flush();
 
-        Debug.Assert(dUnitCost > 1, "Message will NOT appear");
-        Debug.Assert(dUnitCost < 1, "Message will appear since dUnitcost < 1 is false");
-
-        /* System.Xml.XmlDocument oxml = new System.Xml.XmlDocument();
-        Debug.WriteLine(oxml); */
-
-        TextWriterTraceListener tr1 = new TextWriterTraceListener(System.Console.Out);
-        // Debug.Listeners.Add(tr1);
-        Trace.Listeners.Add(tr1);
-
-        TextWriterTraceListener tr2 = new TextWriterTraceListener(
-            System.IO.File.CreateText("Output.txt")
-        );
-        // Debug.Listeners.Add(tr2);
-        Trace.Listeners.Add(tr2);
-
-        Debug.Unindent();
-        Debug.WriteLine("Debug Information-Product Ending");
-        Debug.Flush();
+        Console.ReadLine();
     }
 }
